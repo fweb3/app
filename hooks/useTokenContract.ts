f323b77469d9e092b055300386d76e6dbcd5f313
Ryan Lewis <aarchaeopteryxx@gmail.com>
2022-05-12 11:12:49 -0700

    Cleanup Repository (#6)
    * merge with main
    
    * rebase
    
    * remove contracts, remove unused deps, update readme
    
    * update lock

--- hooks/useTokenContract.ts
+++ hooks/useTokenContract.ts
@@ -1,7 +1,6 @@
-import ERC20_ABI from "../contracts/ERC20.json";
-import type { ERC20 } from "../contracts/types";
+import ERC20_ABI from "./Fweb3Token.json";
 import useContract from "./useContract";
 
 export default function useTokenContract(tokenAddress?: string) {
-  return useContract<ERC20>(tokenAddress, ERC20_ABI);
+  return useContract(tokenAddress, ERC20_ABI.abi);
 }
