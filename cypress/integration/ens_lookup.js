a070014b13c11a60b2adf5067e7b8c0eb49e5915
Ryan Lewis <aarchaeopteryxx@gmail.com>
2022-05-12 09:35:39 -0700

    Moved vercel build to this org repo. Testing deploy pipeline (#5)
    * add new faucet addresses
    
    * update faucet addresses
    
    * update amounts for faucets
    
    * add checks for both faucets (#186)
    
    Co-authored-by: archae0pteryx <aarchaeopteryxx@gmail.com>
    
    * Update game text around faucets and fix faucet bug (#185)
    
    * update amounts for faucets
    
    * add checks for both faucets (#186)
    
    Co-authored-by: archae0pteryx <aarchaeopteryxx@gmail.com>
    
    Co-authored-by: archae0pteryx <aarchaeopteryxx@gmail.com>
    
    * Update index.tsx (#187)
    
    Updated tooltips to show the current amount that the faucets are providing (hover-over the buttons)
    
    * Fixed lights 3,4,6 (#188)
    
    Fixed "lights" 3, 4, and 6 by doing the following:
    Corrected burn address
    Added polygon api to retrieve internal transactions for the wallet
    Checked for receiving MATIC from faucet inside the internal transactions
    Fixed case sensitivity matching
    
    * change filename for redeploy test
    
    * increase wait time for ens resolution
    
    Co-authored-by: BrianIsMeta <brian.ismeta@gmail.com>

--- cypress/integration/ens_lookup.js
+++ cypress/integration/ens_lookup.js
@@ -9,7 +9,7 @@ describe("ENS Lookup Tests", () => {
   it("load the page for an address known to have an ENS name", () => {
     cy.visit("/?wallet=0x147a0B6E848109D438445bA750645bCc37CbA825");
     // give it 6 seconds before checking since lookups can take a few seconds sometimes
-    cy.wait(6000);
+    cy.wait(10000);
     cy.get("nav > a > p").invoke("text").should("eq", "jovanjester.eth");
   });
 });
