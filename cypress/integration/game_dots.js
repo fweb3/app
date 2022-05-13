7b366fa86f04c9d5ba75b115f6f2d92fe7986780
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-05-13 13:38:43 -0700

    remove old file

--- cypress/integration/game_dots.js
+++ cypress/integration/game_dots.js
@@ -2,6 +2,7 @@ const WIN_STATE = {
   hasWonGame: true,
   trophyId: "1",
 };
+
 const DEFAULT_STATE = {
   hasEnoughTokens: false,
   hasUsedFaucet: false,
@@ -13,6 +14,7 @@ const DEFAULT_STATE = {
   hasMintedNFT: false,
   hasWonGame: false,
 };
+
 describe("Game completion dots", () => {
   it("load the default page", () => {
     cy.visit("/");
@@ -24,7 +26,6 @@ describe("Game completion dots", () => {
   it("loads a wallet that is complete", () => {
     cy.intercept("/api/polygon?**", WIN_STATE);
     cy.visit("/?wallet=0x2A9d8CfD86796E6A68AF9c83FD90F67CcaF1352c");
-    cy.wait(1000);
     cy.get(".game-tile.completed").should("have.length", 9);
   });
 
