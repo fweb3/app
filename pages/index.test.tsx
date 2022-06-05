f5355005a89fdd8770845562a970590cc350a503
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-06-04 18:03:48 -0700

    add index page test

---
+++ pages/index.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import { IComponentProps } from '../types'
+import IndexPage from './index.page'
+
+jest.mock('../components/GameView/ContentSection', () => ({
+  ContentSection: () => <div>foo</div>,
+}))
+
+jest.mock('../components/Chest/ChestSection', () => ({
+  ChestSection: () => <div>bar</div>,
+}))
+
+jest.mock('../components/shared/Layout', () => ({
+  Layout: (props: IComponentProps) => <div>{props.children}</div>,
+}))
+
+describe('app', () => {
+  it('renders without crashing', () => {
+    render(<IndexPage />)
+    expect(screen.getByText('foo')).toBeTruthy()
+    expect(screen.getByText('bar')).toBeTruthy()
+  })
+})
